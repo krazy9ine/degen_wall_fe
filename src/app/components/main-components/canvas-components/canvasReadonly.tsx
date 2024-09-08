9b140c5291b6a18e7a6136fbb13f3a66dd8c6b93
VACostin <virlan.costin.andrei@gmail.com>
2024-09-08 19:53:48 +0300

    Reverted to div of divs cuz canvas slow as shit on mouse pointer

--- src/app/components/main-components/canvas-components/canvasReadonly.tsx
+++ src/app/components/main-components/canvas-components/canvasReadonly.tsx
@@ -1,69 +1,42 @@
-import { useEffect, useRef } from "react";
+"use client";
+
 import { CanvasReadonlyProps, Socials } from "@/app/types";
-import { PX_HEIGHT, PX_WIDTH, SQUARE_BORDER_COLOR } from "@/app/constants";
+import SquareReadonly from "./squareReadonly";
+import { PX_HEIGHT, PX_WIDTH } from "@/app/constants";
 
 export default function CanvasReadonly(
   props: CanvasReadonlyProps & { onSetSocials: (socials: Socials) => void }
 ) {
-  const {
-    squareSize,
-    isEditMode,
-    canvasReadonly: canvasLayout,
-    onSetSocials,
-  } = props;
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    if (!canvas) return;
-    const context = canvas.getContext("2d");
-    if (!context) return;
-
-    canvas.width = PX_WIDTH * squareSize;
-    canvas.height = PX_HEIGHT * squareSize;
-
-    canvasLayout.forEach((pixel, index) => {
-      const row = Math.floor(index / PX_WIDTH);
-      const col = index % PX_WIDTH;
-      context.fillStyle = `#${pixel.color}`;
-      context.fillRect(
-        col * squareSize,
-        row * squareSize,
-        squareSize,
-        squareSize
-      );
-      context.strokeStyle = `#${SQUARE_BORDER_COLOR}`;
-      context.strokeRect(
-        col * squareSize,
-        row * squareSize,
-        squareSize,
-        squareSize
-      );
-    });
-  }, [canvasLayout, squareSize]);
-
-  const handleMouseMove = (e: React.MouseEvent<HTMLCanvasElement>) => {
-    const canvas = canvasRef.current;
-    if (!canvas) return;
-    const rect = canvas.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-
-    const colIndex = Math.floor(x / squareSize);
-    const rowIndex = Math.floor(y / squareSize);
-    const index = rowIndex * PX_WIDTH + colIndex;
-    const pixel = canvasLayout[index];
-
-    if (pixel) {
-      onSetSocials(pixel.socials);
-    }
-  };
-
+  const { squareSize, isEditMode, canvasReadonly: canvasLayout, onSetSocials } = props;
   return (
-    <canvas
-      ref={canvasRef}
-      style={{ opacity: isEditMode ? 0 : 1, display: "block" }}
-      onMouseMove={handleMouseMove}
-    />
+    <div
+      id="canvas-view"
+      className="flex"
+      style={{ opacity: isEditMode ? 0 : 1 }}
+    >
+      <div id="canvas-readonly">
+        {Array.from({ length: PX_HEIGHT }).map((_, rowIndex) => (
+          <div
+            key={`row-${rowIndex}`}
+            style={{
+              display: "flex",
+            }}
+          >
+            {Array.from({ length: PX_WIDTH }).map((_, colIndex) => {
+              const index = rowIndex * PX_WIDTH + colIndex;
+              const pixel = canvasLayout[index];
+              return (
+                <SquareReadonly
+                  key={`${rowIndex}-${colIndex}`}
+                  size={squareSize}
+                  metadataItem={pixel}
+                  onSetSocials={onSetSocials}
+                />
+              );
+            })}
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }

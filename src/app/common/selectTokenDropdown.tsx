422e7408210caf383bb08901a3e2bf445e5fcd38
VACostin <virlan.costin.andrei@gmail.com>
2024-09-10 20:12:48 +0300

    Now dropdown has images

--- src/app/common/selectTokenDropdown.tsx
+++ src/app/common/selectTokenDropdown.tsx
@@ -1,20 +1,58 @@
 import { useContext } from "react";
 import { SelectTokenContext } from "../context/SelectTokenProvider";
-import Dropdown from "react-dropdown";
-import "react-dropdown/style.css";
+import { Dropdown } from "primereact/dropdown";
 import { TOKEN_DICT } from "../constants";
 import { TokenSymbol } from "../types";
 
+// Custom option template to render image and label in the dropdown
+const optionTemplate = (option: TokenSymbol) => {
+  return (
+    <div className="flex align-items-center">
+      <img
+        alt={option}
+        src={`/${option}.png`}
+        className="mr-2"
+        style={{ width: "20px", height: "20px" }}
+      />
+      <span>{option}</span>
+    </div>
+  );
+};
+
+// Custom value template to render image and label in the input box
+const valueTemplate = (option: TokenSymbol) => {
+  if (option) {
+    return (
+      <div className="flex align-items-center">
+        <img
+          alt={option}
+          src={`/${option}.png`}
+          className="mr-2"
+          style={{ width: "20px", height: "20px" }}
+        />
+        <span>{option}</span>
+      </div>
+    );
+  }
+  return <span>Select a token</span>; // Placeholder when no token is selected
+};
+
 export default function SelectTokenDropdown() {
   const context = useContext(SelectTokenContext);
-  const handleChange = (selectedOption: { value: TokenSymbol }) => {
-    context?.onSetToken(selectedOption.value);
+
+  const handleChange = (e: any) => {
+    console.log(e);
+    context?.onSetToken(e.value);
   };
+
   return (
     <Dropdown
+      value={context.symbol} // Find the selected token option
       options={Object.keys(TOKEN_DICT)}
-      value={context?.symbol} //@ts-ignore
       onChange={handleChange}
-    ></Dropdown>
+      optionLabel="label"
+      itemTemplate={optionTemplate} // Template for dropdown options
+      valueTemplate={valueTemplate} // Template for selected value in the input box
+    />
   );
 }

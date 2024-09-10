17d4396ae500e4bfdab1ad4a8ca1d9cf38169742
VACostin <virlan.costin.andrei@gmail.com>
2024-09-10 21:22:44 +0300

    Removed 0 balance bs

--- src/app/web3/updateBalance.ts
+++ src/app/web3/updateBalance.ts
@@ -16,7 +16,6 @@ export const updateBalance = async (
   onSetBalance: (balance: number) => void
 ) => {
   try {
-    onSetBalance(0);
     const currentWalletAddress = owner.toString();
     const { address: mint, decimals } = token;
     const balanceCacheString = localStorage.getItem(mint);
